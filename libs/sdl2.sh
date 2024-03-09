fedd787febc3baa9ceb9ed07e5d8869a079ba6ea
mtdcy <mtdcy.chen@gmail.com>
2024-03-09 16:45:11 +0800

    upgrade sdl2

--- libs/sdl2.sh
+++ libs/sdl2.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-upkg_lic=""
-upkg_ver=2.0.9
-upkg_url=https://libsdl.org/release/SDL2-$upkg_ver.tar.gz
-upkg_sha=255186dc676ecd0c1dbf10ec8a2cc5d6869b5079d8a38194c2aecdff54b324b1
+upkg_lic="Zlib"
+upkg_ver=2.30.1
+upkg_url=https://github.com/libsdl-org/SDL/releases/download/release-$upkg_ver/SDL2-$upkg_ver.tar.gz
+upkg_sha=01215ffbc8cfc4ad165ba7573750f15ddda1f971d5a66e9dcaffd37c587f473a
 
 upkg_static() {
     rm CMakeLists.txt 
 
     upkg_args=(
         --without-x
-        --enable-shared 
-        --enable-rpath
+        --disable-shared 
         --enable-static 
         )
 
-    upkg_configure "${upkg_args[@]}" --disable-shared && 
-    upkg_make_njobs install 
-    return $?
+    upkg_configure && upkg_make_njobs && upkg_make install 
 }
 
