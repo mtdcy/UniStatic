956b7f0f075cdad629828ab0fa4c5b7eeea070a1
mtdcy <mtdcy.chen@gmail.com>
2024-03-09 20:02:01 +0800

    upgrade lzma

--- libs/lzma.sh
+++ libs/lzma.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
+# General-purpose data compression with high compression ratio
 
-upkg_lic=""
-upkg_ver=5.2.4
-upkg_url=https://sourceforge.net/projects/lzmautils/files/xz-5.2.4.tar.bz2
-upkg_sha=3313fd2a95f43d88e44264e6b015e7d03053e681860b0d5d3f9baca79c57b7bf
+upkg_lic="BSD"
+upkg_ver=5.6.0
+upkg_url=https://github.com/tukaani-project/xz/releases/download/v$upkg_ver/xz-$upkg_ver.tar.gz
+upkg_sha=0f5c81f14171b74fcc9777d302304d964e63ffc2d7b634ef023a7249d9b5d875
 
 upkg_args=(
+    --disable-option-checking
+    --enable-silent-rules
+    --disable-nls
     --disable-debug
-    --enable-shared 
+    --disable-shared 
     --enable-static
 )
 
 upkg_static() {
-    upkg_configure "${upkg_args[@]}" --disable-shared &&
-    upkg_make_njobs install &&
-    upkg_make_test check
-    return $?
+    rm CMakeLists.txt # force use configure 
+
+    upkg_configure && upkg_make_njobs && upkg_make install && upkg_make_test check
 }
